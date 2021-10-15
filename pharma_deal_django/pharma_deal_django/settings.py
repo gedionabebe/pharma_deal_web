4954bfff8b7de99cadadb1f7595308a9dda24708
NahomTadesse <nahitad12@gmail.com>
2021-10-15 12:52:58 +0300

    fix

--- pharma_deal_django/pharma_deal_django/settings.py
+++ pharma_deal_django/pharma_deal_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1zmze_a40k7d-70e3@alxsfcgyd59e-axg*q2jxocfq%k$&vq0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
